c42c8a766b80eb4fc7db8f66437e13f5f57f0223
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-05-02 22:41:33 +0530

    Completed index page

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const port = 7000;
 
+//main router to handle requests
 const urlRoute = require("./routes/url")
 
+//mongodb connection
 const { connectToMongoDB } = require("./connectMongoDB")
 connectToMongoDB("mongodb://127.0.0.1:27017/UrlShortener")
     .then(() => console.log("mongoDB connected"))
 
+//json middleware for body parsing
 app.use(express.json())
-app.use("/url", urlRoute)
 
+//sending all requests to the router
+app.use("/", urlRoute)
 
 app.listen(port, () => console.log(`Server started at port ${port}`))
