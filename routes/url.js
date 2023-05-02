96d685a46ebed2df82a756c29a2cc257d2f2017e
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-05-02 22:44:03 +0530

    Router completed

--- routes/url.js
+++ routes/url.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router();
 
-const { handlegenerateNewShortURL, handleRedirectShortId } = require("../controllers/url.js")
+//importing controller functions for handling
+const {
+    handlegenerateNewShortURL,
+    handleRedirectShortId,
+    handleGetAnalytics
+} = require("../controllers/url.js")
 
 
+//routing to specific controller functions
 router.post("/", handlegenerateNewShortURL);
 router.get("/:shortId", handleRedirectShortId);
+router.get("/analytics/:shortId", handleGetAnalytics)
 
 module.exports = router;
